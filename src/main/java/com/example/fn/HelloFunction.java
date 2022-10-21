a356c98db4b1209e7920366083d7e9a038c58e3c
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-10-21 18:21:37 +0530

    fix

--- src/main/java/com/example/fn/HelloFunction.java
+++ src/main/java/com/example/fn/HelloFunction.java
@@ -22,8 +22,9 @@ public class HelloFunction {
     public String handleRequest(String input) throws SQLException {
         String name = (input == null || input.isEmpty()) ? "tamo-iot" : input;
 
-        String csvFilePath = "/function/csv/iot1.csv";
-        File file = new File(csvFilePath);
+        String csvFilePath = "iot1.csv";
+        File homedir = new File(System.getProperty("user.home"));
+        File file = new File(homedir, csvFilePath);
         String csvFileUrl = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/n5odYj5P7tXVIb3X13wUamCIU0-BtiMif9rT-stBk_LEzp93xxgwFziQEF2cAP0u/n/sehubjapacprod/b/tamo-input-iot-files/o/people.csv";
 
         System.out.println("----------------------------1");
